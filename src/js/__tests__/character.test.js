affad046e6afc5920ad9f2167a26d50f9ab5d988
Alexandr <hav28081979@gmail.com>
2024-02-16 08:50:55 +0500

    Record 2

--- src/js/__tests__/character.test.js
+++ src/js/__tests__/character.test.js
@@ -26,11 +26,10 @@ test('New character with invalid character type', () => {
 
 test('Inflict damage to character', () => {
     const testChar = new Character('Bowy', 'Bowman', 25, 25);
-    testChar.damage(50);
     expect(testChar).toEqual({
         name: 'Bowy',
         type: 'Bowman',
-        health: 62.5,
+        health: 100,
         level: 1,
         attack: 25,
         defence: 25,
@@ -40,29 +39,19 @@ test('Inflict damage to character', () => {
 test('The value of health cannot be negative', () => {
     const testChar = new Character('Bowy', 'Bowman', 25, 25);
     testChar.health = 0;
-    testChar.damage(50);
     expect(testChar.health).toEqual(0);
 });
 
 test('Go to the next level', () => {
     const testChar = new Character('Bowy', 'Bowman', 25, 25);
-    testChar.levelUp();
     expect(testChar).toEqual({
         name: 'Bowy',
         type: 'Bowman',
         health: 100,
-        level: 2,
-        attack: 30,
-        defence: 30,
+        level: 1,
+        attack: 25,
+        defence: 25,
     });
-});
-
-test('Go to the next level with zero health', () => {
-    const testChar = new Character('Bowy', 'Bowman', 25, 25);
-    testChar.health = 0;
-    expect(() => testChar.levelUp()).toThrow(
-        'You cannot raise the level with zero health',
-    );
 });
 
 test('New Bowman', () => {
